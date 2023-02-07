b51537ec8d6990c7205a53591acc95096907fe0a
GuillaumeLeBerreBIT <guillaume.le.berre@student.howest.be>
2023-02-07 16:35:46 -0500

    Need to complete the dictionary module as for the main code need to make the folders have specific names

---
+++ MangaDict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+##############################################
+# DIFFERENT MANGAS
+##############################################
+# File containing the different links of the Mangas
+
+def MangaLinks(link):
+
+    if link == "AOT" or link == "AttackOnTitan":
+        url = 'https://ww8.readsnk.com/chapter/shingeki-no-kyojin-chapter-'
+    
+    return url
